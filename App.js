68946560b28ecafcabaffc4d7ec287f5353524d5
Mritunjay Pathak <cybermritunjay@gmail.com>
2020-04-29 00:42:53 +0530

    implemented net info without app crashing

--- App.js
+++ App.js
@@ -5,8 +5,11 @@ import * as Font from 'expo-font';
 import { PersistGate } from 'redux-persist/lib/integration/react';
 import { Provider } from "react-redux";
 import { Ionicons } from '@expo/vector-icons';
+import NetInfo from '@react-native-community/netinfo';
+
 import Navigation from './common/navigation/navigation'
 import {store,persistor} from './common/store'
+import { Alert } from 'react-native';
 export default class App extends React.Component {
   constructor(props) {
     super(props);
@@ -14,11 +17,49 @@ export default class App extends React.Component {
         isReady: false,
     };
 }
+checkInterNetConnection = () => {
+  NetInfo.fetch().then(state => {
+    console.log("Connection type", state.type);
+    console.log("Is connected?", state.isConnected);
+    if(! state.isConnected) {{
+    Alert.alert(
+      'No Internet',
+      'You Do Not Have Internet Connectivity!!',
+      [
+        {
+          text: 'Retry',
+          onPress: () => this.checkConnection(),
+          style: 'cancel',
+        },
+      ],
+      { cancelable: false }
+    );
+  }
+  });
+}
 async componentDidMount() {
   await Font.loadAsync({
       Roboto: require('native-base/Fonts/Roboto.ttf'),
       Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
       ...Ionicons.font,
+  });
+  NetInfo.addEventListener(state => {
+    console.log("Connection type", state.type);
+    console.log("Is connected?", state.isConnected);
+    if(state.isConnected != true){
+      Alert.alert(
+        'No Internet',
+        'You Do Not Have Internet Connectivity!! Kindly Try Again',
+        [
+          {
+            text: 'Retry',
+            onPress: () => this.checkConnection(),
+            style: 'cancel',
+          },
+        ],
+        { cancelable: false }
+      );
+    }
   });
 this.setState({ isReady: true });
 }

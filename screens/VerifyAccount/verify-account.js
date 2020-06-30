00c1916e60e6e9d4e29c0e172e976407dfb52675
Mritunjay Pathak <cybermritunjay@gmail.com>
2020-07-01 00:05:50 +0530

    Before Eject final push

--- screens/VerifyAccount/verify-account.js
+++ screens/VerifyAccount/verify-account.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Image, View, Alert, BackHandler, TouchableOpacity } from 'react-native';
 import { Container, Content, Button, Text, Form, Spinner } from 'native-base';
 import styles from './styles';
-import { verifyUser,resendOTP } from '../../services/Auth/actions'
+import { verifyUser, resendOTP } from '../../services/Auth/actions'
 import FormMessage from '../../common/components/FormMessage/form-message';
 import OtpInputs from './components/otp-input'
 import Timer from './components/timer'
@@ -11,25 +11,25 @@ const VerifyUser = (props) => {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
   const [OTP, setOTP] = useState("")
-  const [enableResend,setEnableResend] = useState(true)
-  const [otpLoader,setOtpLoader] = useState(false)
+  const [enableResend, setEnableResend] = useState(true)
+  const [otpLoader, setOtpLoader] = useState(false)
 
   useEffect(() => {
-    const backHandler = BackHandler.addEventListener('hardwareBackPress',()=>props.navigation.navigate('Register'))
-    return ()=> backHandler.remove()
-  },[])
-  const ResendOTPHandler = ()=>{
+    const backHandler = BackHandler.addEventListener('hardwareBackPress', () => props.navigation.navigate('Register'))
+    return () => backHandler.remove()
+  }, [])
+  const ResendOTPHandler = () => {
     setOtpLoader(true)
-    resendOTP({email:props.route.params.email,mobile:props.route.params.mobile,otp:props.route.params.otp}).then(res=>{
-      if(!res.err){
+    resendOTP({ email: props.route.params.email, mobile: props.route.params.mobile, otp: props.route.params.otp }).then(res => {
+      if (!res.err) {
         setEnableResend(false)
         setOtpLoader(false)
       }
     }).catch(err => {
-        setOtpLoader(false)
+      setOtpLoader(false)
       //console.log(err)})
-  })
-}
+    })
+  }
   const getOTP = () => {
     return OTP
   }
@@ -67,7 +67,7 @@ const VerifyUser = (props) => {
     <Container>
       <Content contentContainerStyle={styles.container}>
         <View style={{ alignItems: 'center' }}>
-        <Text>Your Account Hasbeen Created. Kindly Verify Your Account.</Text>
+          <Text>Your Account Hasbeen Created. Kindly Verify Your Account.</Text>
           <Text>A OTP Ha Been Sent To Your Email and Mobile.</Text>
           <Image
             style={{ width: 100, height: 100 }}
@@ -91,12 +91,12 @@ const VerifyUser = (props) => {
                 <Text style={styles.buttonText}>Verify</Text>
               </Button>
             )}
-            {otpLoader?(<Spinner size='small' color="#000000" />):(enableResend?(<TouchableOpacity style={{cursor:'pointer'}} onPress={()=> ResendOTPHandler()} >
-              <Text style={{color:'blue'}}>Resend OTP</Text></TouchableOpacity>
-              ):(
+          {otpLoader ? (<Spinner size='small' color="#000000" />) : (enableResend ? (<TouchableOpacity style={{ cursor: 'pointer' }} onPress={() => ResendOTPHandler()} >
+            <Text style={{ color: 'blue' }}>Resend OTP</Text></TouchableOpacity>
+          ) : (
               <Timer minutes="1" seconds="0" over={enableResend} setOver={setEnableResend} />
-              ))}
-            
+            ))}
+
         </View>
       </Content>
     </Container>
